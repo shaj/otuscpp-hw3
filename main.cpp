7aab45ebf196e49a6ad2885c8fd6ff0a04fdb669
Andrey Shaj <kolobrod2002@yandex.ru>
2018-03-23 17:58:36 +0300

    cmake

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 
+
+
+
 #include <iostream>
 #include <vector>
 #include <map>
 
+#include "allocator.h"
 #include "mylist.h"
 
 long double fact(int N)
@@ -17,50 +21,6 @@ long double fact(int N)
 
 
 
-
-
-template<typename T>
-struct logging_allocator
-{
-	using value_type = T;
-	using pointer = T*;
-	using const_pointer = const T*;
-	using reference = T&;
-	using const_reference = const T&;
-
-	template<typename U>
-	struct rebind
-	{
-		using other = logging_allocator<U>;
-	};
-
-	T *allocate(std::size_t n)
-	{
-		std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
-		auto p = std::malloc(n * sizeof(T));
-		if(!p) throw std::bad_alloc();
-		return reinterpret_cast<T *>(p);
-	}
-
-	void deallocate(T *p, std::size_t n)
-	{
-		std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
-		std::free(p);
-	}
-
-	template<typename U, typename ...Args>
-	void construct(U *p, Args &&...args)
-	{
-		std::cout << __PRETTY_FUNCTION__ << std::endl;
-		new(p) U(std::forward<Args>(args)...);
-	}
-
-	void destroy(T *p)
-	{
-		std::cout << __PRETTY_FUNCTION__ << std::endl;
-		p->~T();
-	}
-};
 
 
 
@@ -120,5 +80,4 @@ int main (int, char *[])
 
 	return 0;
 }
-
 
